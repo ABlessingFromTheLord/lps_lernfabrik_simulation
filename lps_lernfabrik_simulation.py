f0074917aa5fc1e2f2117b43f720c5fdb7e9a2b4
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2024-01-09 23:37:01 +0100

    implemented ruestung_zeit function

--- lps_lernfabrik_simulation.py
+++ lps_lernfabrik_simulation.py
@@ -18,23 +18,15 @@ REPAIR_ZEIT = 10
 PROZESS_ZEIT = 10
 MTTR = 10
 
-# simpy environment decleration
-env = simpy.Environment()
-
-# machines as simpy resources
-machine_jaespa = simpy.Resource(env)
-machine_gz200_1 = simpy.Resource(env)
-machine_gz200_2 = simpy.Resource(env)
-machine_fz12 = simpy.Resource(env)
-machine_arbeitsplatz_2 = simpy.Resource(env)
-
 
 # the factory implementation
 class Lernfabrik:
     # this class simulates all processes taking place in the factory
     def __init__(self, env):
-        self.env = env
-        self.kaputt = False
+        self.env = env # environment variable
+        self.kaputt = False # boolean for denoting when a machine is broken # TODO: check how to optimise
+        self.previously_created = "" # string to denote the previously created part
+        self.next_created = " " # string to denote the next created part
 
     # processes
     def saegen(self):
@@ -120,11 +112,77 @@ class Lernfabrik:
                 required_machines.append(machine)
 
         # TODO: check if the required machines are same as the machines available
+        # TODO:: reformat into taking the shared resources
 
 
     # TODo: ruestung function; takes in previous process and
-    # returns the time needed till machines are equipped
+    def ruestung_zeit(self, machine):
+        # returns the equipping time in minutes as integer
+        if machine == machine_fz12:
+            return 30
 
+        elif (machine == machine_gz200_1) or (machine == machine_gz200_2):
+            if (self.previously_created == "Oberteil") and (self.next_created == "Oberteil"):
+                return 0
+            elif (self.previously_created == "Oberteil") and (self.next_created == "Unterteil"):
+                return 45
+            elif (self.previously_created == "Oberteil") and (self.next_created == "Halteteil"):
+                return 40
+            elif (self.previously_created == "Oberteil") and (self.next_created == "Ring"):
+                return 45
+
+            elif (self.previously_created == "Unterteil") and (self.next_created == "Oberteil"):
+                return 45
+            elif (self.previously_created == "Unterteil") and (self.next_created == "Unterteil"):
+                return 0
+            elif (self.previously_created == "Unterteil") and (self.next_created == "Halteteil"):
+                return 40
+            elif (self.previously_created == "Unterteil") and (self.next_created == "Ring"):
+                return 45
+
+            elif (self.previously_created == "Halteteil") and (self.next_created == "Oberteil"):
+                return 40
+            elif (self.previously_created == "Halteteil") and (self.next_created == "Unterteil"):
+                return 40
+            elif (self.previously_created == "Halteteil") and (self.next_created == "Halteteil"):
+                return 0
+            elif (self.previously_created == "Halteteil") and (self.next_created == "Ring"):
+                return 45
+
+            elif (self.previously_created == "Ring") and (self.next_created == "Oberteil"):
+                return 45
+            elif (self.previously_created == "Ring") and (self.next_created == "Unterteil"):
+                return 45
+            elif (self.previously_created == "Ring") and (self.next_created == "Halteteil"):
+                return 45
+            elif (self.previously_created == "Ring") and (self.next_created == "Ring"):
+                return 0
+
+        elif machine == machine_jaespa:
+            return 0
+
+
+# simpy environment decleration
+env = simpy.Environment()
+
+# instantiate machines as simpy resources
+machine_jaespa = simpy.Resource(env)  # Maschine zum Säegen
+machine_gz200_1 = simpy.Resource(env)  # Machine zum Drehen
+machine_gz200_2 = simpy.Resource(env)  # Machine zum Drehen
+machine_fz12 = simpy.Resource(env)  # Machine zum Fräsen
+machine_arbeitsplatz_2 = simpy.Resource(env)  # Machine zum Montage
+
+# Instantiate LPS Lernfabrik
+fabric = Lernfabrik(env)
+
+# instantiate object of Lernfabrik class
+# test ruestung_zeit
+fabric = Lernfabrik(env)
+fabric.previously_created = "Unterteil"
+fabric.next_created = "Halteteil"
+
+print(fabric.ruestung_zeit(machine_fz12))
+# end of test ruestung_zeit
 
 # running simulation
 env.run()
