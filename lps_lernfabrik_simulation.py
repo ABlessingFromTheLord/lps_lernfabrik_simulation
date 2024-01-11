acbdd4cb4a556e24d5e43e870977f2f6080074e4
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2024-01-11 15:11:45 +0100

    implemented helper functions and abstracted all operations into one
    -> get_operation_time
    -> select_machines

--- lps_lernfabrik_simulation.py
+++ lps_lernfabrik_simulation.py
@@ -13,10 +13,9 @@ MONTAGE_ZEIT = 10  # IN MINUTES
 KLEBEN_ZEIT = 10  # IN MINUTES
 PRUEFEN_ZEIT = 10  # IN MINUTES
 VERPACKEN_ZEIT = 10  # IN MINUTES
-KAPUTT_MACHINE_A = 10
-KAPUTT_MACHINE_B = 10
-KAPUTT_MACHINE_C = 10
-KAPUTT_MACHINE_D = 10
+JAESPA_MZ = 0.98
+GZ_200_MZ = 0.85
+FZ12_MZ = 0 # TODO: get value
 REPAIR_ZEIT = 10
 PROZESS_ZEIT = 10
 MTTR = 10
@@ -28,16 +27,64 @@ RING = 0
 # ORDERS
 ORDERS = []
 
+
+# global helper functions
+def get_operation_time(machine):
+    #  returns the time that a machine(s) performs a certain operation
+    if machine == machine_jaespa:
+        return SAEGEN_ZEIT
+    elif machine == machine_gz200:
+        return DREH_ZEIT
+    elif machine == machine_fz12:
+        return FRAESEN_ZEIT
+    elif (machine[0] == machine_arbeitsplatz) & (machine[1] == machine_gz200):
+        return SENK_ZEIT
+
+
 # the factory implementation
+def select_machines(machines):
+    # receives an array of the machines in the factory
+    # returns true if the required resources are free
+    required_machines = []
+
+    for machine in machines:
+        if not machine.working:
+            required_machines.append(machine)
+
+    # TODO: check if the required machines are same as the machines available
+    # TODO:: reformat into taking the shared resources
+
+
 class Lernfabrik:
     # this class simulates all processes taking place in the factory
-    def __init__(self, env):
-        self.env = env # environment variable
-        self.kaputt = False # boolean for denoting when a machine is broken # TODO: check how to optimise
-        self.previously_created = "" # string to denote the previously created part
-        self.next_created = " " # string to denote the next created part
+    def __init__(self, env, time_run):
+        self.env = env  # environment variable
+        self.kaputt = False  # boolean for denoting when a machine is broken # TODO: check how to optimise
+        self.previously_created = ""  # string to denote the previously created part
+        self.next_created = ""  # string to denote the next created part
+        self.time_run = time_run  # time in which the simulation is left to run
 
-    # processes
+    # operation
+    def operation(self, machine):
+        #  simulates an operation, it is an abstract function
+        #  to know the exact operation executing, look at the time used
+        #  for example, if SAEGEN_ZEIT is used then the process is saegen
+        while True:
+            PROZESS_ZEIT = SAEGEN_ZEIT
+            start = self.env.now
+            try:
+                yield self.env.timeout(get_operation_time(machine))
+
+            except simpy.Interrupt:
+                self.kaputt = True
+                PROZESS_ZEIT -= (self.env.now - start)
+
+                # repairing
+                yield self.env.timeout(self.get_machine_broken_time(machine))  # broken time
+                yield self.env.timeout(60)  # repair time
+                # TODO: change factor to 60 in simulation time, and above to 1
+                self.kaputt = False
+
     def saegen(self):
         #  simulates the "saegen" operation
         while True:
@@ -167,26 +214,13 @@ class Lernfabrik:
                 self.kaputt = False
 
     # Helper functions
-    def select_machines(self, machines):
-        # receives an array of the machines in the factory
-        # returns true if the required resources are free
-        required_machines = []
-
-        for machine in machines:
-            if not machine.working:
-                required_machines.append(machine)
-
-        # TODO: check if the required machines are same as the machines available
-        # TODO:: reformat into taking the shared resources
-
-
     # TODo: ruestung function; takes in previous process and
-    def ruestung_zeit(self, machine):
+    def get_ruestung_zeit(self, machine):
         # returns the equipping time in minutes as integer
         if machine == machine_fz12:
             return 30
 
-        elif (machine == machine_gz200_1) or (machine == machine_gz200_2):
+        elif machine == machine_gz200:
             if (self.previously_created == "Oberteil") and (self.next_created == "Oberteil"):
                 return 0
             elif (self.previously_created == "Oberteil") and (self.next_created == "Unterteil"):
@@ -226,6 +260,17 @@ class Lernfabrik:
         elif machine == machine_jaespa:
             return 0
 
+    def get_machine_broken_time(self, machine):
+        # returns the time that a machine is broken
+        # it is calculated by 1 - Maschinenzuverlässigkeit * total simulation time
+        # this is the downtime of a certain machine, repair time is set to a minute
+        if machine == machine_jaespa:
+            return (1 - JAESPA_MZ) * self.time_run
+        elif machine == machine_gz200:
+            return (1 - GZ_200_MZ) * self.time_run
+        elif machine == machine_fz12:
+            return (1 - FZ12_MZ) * self.time_run
+
 
 # simulates the fulfillment of orders
 def fulfill_orders(orders):
@@ -238,9 +283,9 @@ env = simpy.Environment()
 
 # instantiate machines as simpy resources
 machine_jaespa = simpy.Resource(env)  # Maschine zum Säegen
-machine_gz200_1 = simpy.Resource(env)  # Machine zum Drehen
-machine_gz200_2 = simpy.Resource(env)  # Machine zum Drehen
+machine_gz200 = simpy.Resource(env)  # Machine zum Drehen
 machine_fz12 = simpy.Resource(env)  # Machine zum Fräsen
+machine_arbeitsplatz = simpy.Resource(env)  # Machine zum Montage
 machine_arbeitsplatz_2 = simpy.Resource(env)  # Machine zum Montage
 
 # Instantiate LPS Lernfabrik
