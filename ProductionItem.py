95ba327f8d1a4f3e4e6a8240a01b64f9c6883440
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2024-03-11 14:33:48 +0100

    implemented Production item class

---
+++ ProductionItem.py
@@ -0,0 +1,12 @@
+class Order:
+    # a tuple that defines an item in production as well as when it was produced
+    # this could be an Unilokk or part of an Unilokk
+    def __init__(self, amount, production_date):
+        self.__amount = amount
+        self.__production_date = production_date
+
+    def get_amount(self):
+        return self.__amount
+
+    def get_production_date(self):
+        return self.__production_date
